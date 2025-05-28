1e0334c1ca61560d37be6a57794c2a0d3a6c84f0
anshul <anshul4894@gmail.com>
2025-05-29 00:24:18 +0530

    ⛓ Auto-commit from the depths at 2025-05-29 00:24:18

---
+++ OOPS/abstract_data_type.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class stack
+{
+private:
+    vector<int> data; // hidden data storage
+public:
+    void push(int value){
+        data.push_back(value);
+    }
+    int pop(int value){
+        if(isem)
+    }
+};
+int main()
+{
+}
