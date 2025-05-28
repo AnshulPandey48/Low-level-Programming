9535b7f2039dfc1f7ddb480eed94d0dd7a70defc
anshul <anshul4894@gmail.com>
2025-05-29 00:04:03 +0530

    ⛓ Auto-commit from the depths at 2025-05-29 00:04:03

--- OOPS/inlinefun.cpp
+++ OOPS/inlinefun.cpp
@@ -1,19 +1,6 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+inline  
+int main(){
 
-int add(int a, int b)
-{ // Implicitly inline
-    return a + b;
-}
-
-int main()
-{
-    int x, y;
-    cout << "enter: ";
-    cin >> x;
-    cout << "enter: ";
-    cin >> y;
-
-    int result = add(x, y);
-    cout << result << endl;
 }
