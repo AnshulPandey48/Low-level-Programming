48975caa5400b889a48525299b77b0d27ae732db
anshul <anshul4894@gmail.com>
2025-05-31 02:12:35 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 02:12:35

--- OOPS/friend.cpp
+++ OOPS/friend.cpp
@@ -1,9 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class demon{
-int evil = 666;
+class demon
+{
+    int evil = 666;
+    friend void show(demon d);
+};
 
+void show(demon d)
+{
+    cout << d.evil << endl;
 }
-int main(){
-
+int main()
+{
+    demon lucifer;
+    show(lucifer);
+    return 0;
 }
