2d4df56284f5aefdca596118bacdba900893f580
anshul <anshul4894@gmail.com>
2025-05-31 01:52:20 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 01:52:20

---
+++ OOPS/friend.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class devil
+{
+    int secret = 666;
+
+public:
+    friend void reveal(devil d);
+};
+// void function defination
+
+int main()
+{
+}
