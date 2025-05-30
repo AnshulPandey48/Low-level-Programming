b8550c56224a11f1c75021d72d04598fcf2ac355
anshul <anshul4894@gmail.com>
2025-05-31 01:37:10 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 01:37:10

--- OOPS/scoperes.cpp
+++ OOPS/scoperes.cpp
@@ -1,9 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int x = 10; // global 
-int main(){
-int x = 20;
-cout << "local : " << x << endl;
-cout << "global : " << ::x << endl;
-return 0;
+class abc
+{
+public:
+    void show(){
+        cout << "hello " << endl;
+    }
+};
+void abc ::show()
+{
+    cout << "hello bhosad" << endl;
+}
+int x = 10; // global
+int main()
+{
+    int x = 20;
+    cout << "local : " << x << endl;
+    cout << "global : " << ::x << endl;
+    abc a1;
+    a1.show();
+    return 0;
 }
