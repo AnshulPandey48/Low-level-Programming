83b661c1ecf60ee27c59b05700b9196a8ab6551b
anshul <anshul4894@gmail.com>
2025-05-31 03:03:11 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 03:03:11

---
+++ OOPS/constructor1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class abc
+{
+public:
+    void display()
+    {
+        cout << "default constructor" << endl;
+    }
+};
+int main()
+{
+    abc a;
+    a.display();
+    return 0;
+}
---
+++ OOPS/constructor2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class devil
+{
+    int power;
+
+public:
+    void display(int p)
+    {
+        power = p;
+        cout << power << endl;
+    }
+};
+int main()
+{
+    devil db;
+    db.display(100);
+}
