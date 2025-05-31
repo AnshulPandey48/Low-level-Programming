aa7a3b059608e5fa733cb362a4f1af421b4a3c53
anshul <anshul4894@gmail.com>
2025-06-01 02:00:07 +0530

    ⛓ Auto-commit from the depths at 2025-06-01 02:00:07

--- OOPS/aggregation.cpp
+++ OOPS/aggregation.cpp
@@ -13,3 +13,29 @@ public:
         cout << "students : " << name << endl;
     }
 };
+
+class classroom
+{
+public:
+    vector<student *> students;
+    void addstudents(student *s)
+    {
+        students.push_back(s);
+    }
+    void showall()
+    {
+        for (auto s : students)
+        {
+            s->display();
+        }
+    }
+};
+int main(){
+    student s1("lucifer");
+    student s2("belial");
+    classroom c;
+    c.addstudents(&s1);
+    c.addstudents(&s2);
+    c.showall();
+    return 0;
+}
