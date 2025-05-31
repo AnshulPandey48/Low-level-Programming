bc7b49701f40ce287dc8ff05f9ff152da7f8fd1e
anshul <anshul4894@gmail.com>
2025-06-01 02:20:20 +0530

    ⛓ Auto-commit from the depths at 2025-06-01 02:20:20

--- OOPS/composition.cpp
+++ OOPS/composition.cpp
@@ -4,8 +4,29 @@ using namespace std;
 class heart
 {
 public:
-
+    heart()
+    {
+        cout << "heart created " << endl;
+    }
+    ~heart()
+    {
+        cout << "heart destroyed" << endl;
+    }
+};
+class human
+{
+public:
+    heart h;
+    human()
+    {
+        cout << "human created" << endl;
+    }
+    ~human()
+    {
+        cout << "human deleted" << endl;
+    }
 };
 int main()
 {
+    human h1;
 }
