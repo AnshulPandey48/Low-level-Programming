8d8583c43ecf5ff244c60c6f86b26db5e0ac1f4a
anshul <anshul4894@gmail.com>
2025-06-01 01:55:04 +0530

    ⛓ Auto-commit from the depths at 2025-06-01 01:55:04

--- OOPS/aggregation.cpp
+++ OOPS/aggregation.cpp
@@ -3,5 +3,13 @@ using namespace std;
 class student
 {
 public:
-    
-}
+    string name;
+    student(string n)
+    {
+        name = n;
+    }
+    void display()
+    {
+        cout << "students : " << name << endl;
+    }
+};
