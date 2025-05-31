64a7782c2334eab906f2a7b7c4e9d74a51898435
anshul <anshul4894@gmail.com>
2025-05-31 13:42:28 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 13:42:28

--- OOPS/copy constructor.cpp
+++ OOPS/copy constructor.cpp
@@ -5,10 +5,20 @@ class devil
     int *power;
 
 public:
-    devil(int p){
+    devil(int p)
+    {
         power = new int; // power will store the memory address new int is dynamic memory alocate
-        
+        *power = p;      // this will store value of p
     }
+
+    // copy constructor
+    devil(const devil &d)
+    {
+        power = new int;
+        *power = *(d.power);
+        cout << "copy constructor called" << endl;
+    }
+    void show()
 };
 int main()
 {
