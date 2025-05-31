d7b7c89c0529a76683926cca00e2dc3e78c701cd
anshul <anshul4894@gmail.com>
2025-05-31 15:13:49 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 15:13:49

--- OOPS/copy constructor 2 .cpp
+++ OOPS/copy constructor 2 .cpp
@@ -4,16 +4,33 @@ class demon
 {
     int power = 666;
     int result = 0;
+    int limit;
 
 public:
-    demon(int x){
-        for (int  i = 0; i < x; i++)
+    demon(int x)
+    {
+        limit = x;
+        for (int i = 0; i < limit; i++)
         {
             result = power + i;
         }
-        
+    }
+    // copy constructor here
+    demon(demon &x)
+    {
+        for (int i = 0; i < x.limit; i++)
+        {
+            result = power + i;
+        }
+    }
+    void show()
+    {
+        cout << result << endl;
     }
 };
 int main()
 {
+    demon lucifer(4);
+    lucifer.show();
+    demon belial = lucifer(5);
 }
