efc88527a0ab8a12118af03fc6175b62cbcb64e0
anshul <anshul4894@gmail.com>
2025-05-31 14:07:48 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 14:07:48

--- OOPS/copy constructor 2 .cpp
+++ OOPS/copy constructor 2 .cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+class demon{
+    int value;
+    public:
+    demon(int x){
+        int result = value * x;
+    }
+    void show(){
+        cout << result << endl;
+    }
+}
 int main(){
 
 }
