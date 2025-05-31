8e74c9bccbabc35eee0026a079a5b18541f47db2
anshul <anshul4894@gmail.com>
2025-06-01 01:09:35 +0530

    ⛓ Auto-commit from the depths at 2025-06-01 01:09:35

--- OOPS/aggregation.cpp
+++ OOPS/aggregation.cpp
@@ -3,39 +3,5 @@ using namespace std;
 class student
 {
 public:
-    string name;
-    student(string n)
-    {
-        name = n;
-    }
-    void show()
-    {
-        cout << "students : " << name << endl;
-    }
-};
-class classroom
-{
-public:
-    vector<student *> students;
-    void addstudents(student *s) // So students is a vector of pointers to Student objects.
-    {
-        students.push_back(s);
-    }
-    void showstudent()
-    {
-        for (auto s : students)
-        {
-            s->show();
-        }
-    }
-};
-int main()
-{
-    student s1("lucifer");
-    student s2("belial");
-    classroom c;
-    c.addstudents(&s1);
-    c.addstudents(&s2);
-    c.showstudent();
-    return 0;
+    
 }
