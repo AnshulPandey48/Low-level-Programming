0cfef4354a1ea51179b7b4b6a8e7ecbcfdb25215
anshul <anshul4894@gmail.com>
2025-05-31 22:09:48 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 22:09:48

--- OOPS/diamond problem.cpp
+++ OOPS/diamond problem.cpp
@@ -1,8 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class demon{
-    
-}
-int main(){
-    
+class demon
+{
+public:
+    void show()
+    {
+        cout << "hello " << endl;
+    }
+};
+class lucifer : virtual public demon
+{
+public:
+};
+class belial : virtual public demon
+{
+public:
+};
+class child : public lucifer, belial
+{
+public:
+};
+
+int main()
+{
+    child l1;
+    l1.show();
+    return 0;
 }
