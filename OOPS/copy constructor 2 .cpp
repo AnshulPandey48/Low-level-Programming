5c422bf4c5ec9fe49d4ee34bdff4b672d7e10429
anshul <anshul4894@gmail.com>
2025-05-31 14:48:16 +0530

    ⛓ Auto-commit from the depths at 2025-05-31 14:48:16

--- OOPS/copy constructor 2 .cpp
+++ OOPS/copy constructor 2 .cpp
@@ -1,34 +1,17 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-class demon {
+class demon{
     int value = 100;
-    int result;
-
-public:
-    // Constructor
-    demon(int x) {
-        result = value * x;
+    int result = 0;
+    public:
+    demon(int x){
+        result = x + value;
     }
-
-    // Copy Constructor
-    demon(const demon &x) {
-        cout << "copy constructor called: " << endl;
-        value = x.value;
-        result = x.result;  // Copy the result as well
+    // copy constructor
+    demon(demon &x){
+        result = demon.x + value;
     }
+}
+int main(){
 
-    void show() {
-        cout << "Result: " << result << endl;
-    }
-};
-
-int main() {
-    demon lucifer(100);  // result = 100 * 100 = 10000
-    lucifer.show();
-
-    demon belial = lucifer;  // Copy constructor is called here
-    belial.show();           // should print same result
-
-    return 0;
 }
