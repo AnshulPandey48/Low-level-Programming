0d8eed27bdbec5a795dd52f69fe66f59c03dea92
anshul <anshul4894@gmail.com>
2025-06-01 18:19:24 +0530

    ⛓ Auto-commit from the depths at 2025-06-01 18:19:24

--- OOPS/operator overloading2.cpp
+++ OOPS/operator overloading2.cpp
@@ -3,5 +3,13 @@ using namespace std;
 class vectoradd
 {
 public:
+    int x, y;
+    vectoradd(int a = 0, int b = 0)
+    {
+        x = a;
+        y = b;
+    }
+    vectoradd operator +(const&obj)
 
+    }
 };
