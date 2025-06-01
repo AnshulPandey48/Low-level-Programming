892f57385ccd72768e3522de163d51965da741ea
anshul <anshul4894@gmail.com>
2025-06-01 16:26:08 +0530

    ⛓ Auto-commit from the depths at 2025-06-01 16:26:08

--- OOPS/operator overloading.cpp
+++ OOPS/operator overloading.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-class complex{
-
+class complex
+{
+public:
+    int real, imag;
+    complex(int r = 0, int i = 0)
+    {
+        real = r;
+        imag = i;
+    }
+    // operator overloading
+    
 };
 int main()
 {
-
 }
