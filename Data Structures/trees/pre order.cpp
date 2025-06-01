78843b36df77734ba77a1c2e76148d0a9392ce8a
anshul <anshul4894@gmail.com>
2025-06-02 00:07:11 +0530

    ⛓ Auto-commit from the depths at 2025-06-02 00:07:11

---
+++ Data Structures/trees/pre order.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *left, *right;
+    node(int v)
+    {
+        data = v;
+        left = right = nullptr;
+    }
+};
+// pre order traversal -> root , left , right
+void preorder(struct node*head){
+    
+}
+
+int main()
+{
+}
