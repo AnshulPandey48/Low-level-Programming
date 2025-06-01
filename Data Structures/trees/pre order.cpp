c3d3f9b3db8d92559b4af209ac32abca33688aea
anshul <anshul4894@gmail.com>
2025-06-02 00:27:23 +0530

    ⛓ Auto-commit from the depths at 2025-06-02 00:27:23

--- Data Structures/trees/pre order.cpp
+++ Data Structures/trees/pre order.cpp
@@ -1,10 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct node{
+struct node
+{
     int data;
-    
-
+    node *left;
+    node *right;
+    node(int v)
+    {
+        data = v;
+        left = right = nullptr;
+    }
+};
+void preorder(node *head)
+{
+    cout << head->data << " ";
+    preorder(head->left);
+    preorder(head->right);
 }
-int main(){
+int main()
+{
+    struct node *root = new node(1); // ✅ use 'node' not 'Node'
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->right = new node(6);
 
+    preorder(root); // ✅ correct function name
+
+    return 0;
 }
