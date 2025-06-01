25f9a96c039d0e01611dd0d60024e147ed518ff1
anshul <anshul4894@gmail.com>
2025-06-02 00:12:14 +0530

    ⛓ Auto-commit from the depths at 2025-06-02 00:12:14

--- Data Structures/trees/pre order.cpp
+++ Data Structures/trees/pre order.cpp
@@ -11,10 +11,27 @@ struct node
     }
 };
 // pre order traversal -> root , left , right
-void preorder(struct node*head){
-    
+void preorder(struct node *head)
+{
+    if (head == nullptr)
+    {
+        return;
+    }
+    cout << head->data << " ";
+    preorder(head->left);
+    preorder(head->right);
 }
 
 int main()
 {
+    struct node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+
+    printPreorder(root);
+
+    return 0;
 }
