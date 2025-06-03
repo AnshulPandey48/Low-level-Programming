9020f3626c62d9b9b62278ce0b45896b74519b12
anshul <anshul4894@gmail.com>
2025-06-04 01:05:05 +0530

    ⛓ Auto-commit from the depths at 2025-06-04 01:05:05

---
+++ OOPS/test.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+// single inheritance
+class aditya  // parent class
+{
+public:
+    void area()
+    {
+        cout << "aditya is here";
+    }
+};
+class anshul : public aditya
+{
+public:
+    void show()
+    {
+        cout << "Anshuk iss ";
+    }
+};
+
+class chodu : public aditya{
+    
+}
+
+
