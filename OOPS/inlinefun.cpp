9d1968f1f494c9061b6af785fffd377c4592a34b
anshul <anshul4894@gmail.com>
2025-06-04 00:34:46 +0530

    ⛓ Auto-commit from the depths at 2025-06-04 00:34:46

--- OOPS/inlinefun.cpp
+++ OOPS/inlinefun.cpp
@@ -1,6 +1,8 @@
+// classes and objects   class -> blueprint not allocated memory,  
+
 #include <bits/stdc++.h>
 using namespace std;
-inline int add(int a, int b)
+ int add(int a, int b)
 {
     return a + b;
 }
