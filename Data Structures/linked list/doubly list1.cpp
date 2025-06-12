6e4df91c85acfd4c2cf1aef0b746859efe9eda10
anshul <anshul4894@gmail.com>
2025-06-13 01:57:46 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 01:57:46

--- Data Structures/linked list/doubly list1.cpp
+++ Data Structures/linked list/doubly list1.cpp
@@ -11,16 +11,36 @@ struct node
         next = prev = nullptr;
     }
 };
- 
+
 // insertion at front
-node*insertatfirst(node*head,int val){
-    node*newnode = new node(val);
-    newnode -> next = head;
-    if(head != nullptr){
-        head -> prev = newnode;
+node *insertatfirst(node *head, int val)
+{
+    node *newnode = new node(val);
+    newnode->next = head;
+    if (head != nullptr)
+    {
+        head->prev = newnode;
     }
 }
+// insert at the end
+node *insertatend(node *head, int val)
+{
+    node *newnode = new node(val);
+    if (head == nullptr)
+        head = newnode;
+    else
+    {
+
+        node *curr = head;
+        while (curr != nullptr)
+        {
+            curr = curr->next;
+        }
+        curr -> next = newnode;
+        newnode -> prev = curr;
+    }
+}
+
 int main()
 {
-
 }
