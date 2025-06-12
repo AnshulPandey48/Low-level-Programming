07192e46f1c3e7c97e7e9f2f02250df8386a50d7
anshul <anshul4894@gmail.com>
2025-06-13 01:47:38 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 01:47:38

---
+++ Data Structures/linked list/doubly list1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    node *next;
+    node *prev;
+    int data;
+    node(int x)
+    {
+        data = x;
+        next = prev = nullptr;
+    }
+};
+ 
+// insertion at front
+node*insertatfirst(node*head,int val){
+    
+}
+int main()
+{
+
+}
