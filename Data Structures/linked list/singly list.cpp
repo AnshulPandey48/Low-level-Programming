691985f4049d14b591809338fa2789a03e6b5b89
anshul <anshul4894@gmail.com>
2025-06-13 00:00:27 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 00:00:27

--- Data Structures/linked list/singly list.cpp
+++ Data Structures/linked list/singly list.cpp
@@ -1,7 +1,29 @@
-#include<bits        ,z0000000000000000000
-
-jhh1    Q11QW1zNNNNNNNNNNN             /tdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data = x;
+        next = nullptr;
+    }
+};
+// insert at the begining
+node *insertathead(node *head, int val)
+{
+    if (head == nullptr)
+        return;
+    node *newnode = new node(4);
+    newnode->next = head;
+    return newnode;
+}
+// insert at a custom position
+node*insertatpos(node*head,int key ,int val){
+    node*curr = head;
+    
+}
+int main()
+{
 }
