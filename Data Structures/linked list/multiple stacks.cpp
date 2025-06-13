df1849eecfceea8f0f37a6dbb946bda0f1b6f583
anshul <anshul4894@gmail.com>
2025-06-13 21:23:44 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 21:23:44

--- Data Structures/linked list/multiple stacks.cpp
+++ Data Structures/linked list/multiple stacks.cpp
@@ -1,15 +1,68 @@
-   #include <iostream>
-   #include <stack>
+#include <iostream>
+using namespace std;
 
-   int main() {
-       std::stack<int> stack1;
-       std::stack<std::string> stack2;
+#define MAX 1000
 
-       stack1.push(10);
-       stack2.push("hello");
+class TwoStacks {
+    int arr[MAX];
+    int top1, top2;
 
-       std::cout << stack1.top() << std::endl;
-       std::cout << stack2.top() << std::endl;
+public:
+    TwoStacks() {
+        top1 = -1;
+        top2 = MAX;
+    }
 
-       return 0;
-   }
+    // Push in stack 1
+    void push1(int x) {
+        if (top1 < top2 - 1) {
+            arr[++top1] = x;
+        } else {
+            cout << "Stack Overflow\n";
+        }
+    }
+
+    // Push in stack 2
+    void push2(int x) {
+        if (top1 < top2 - 1) {
+            arr[--top2] = x;
+        } else {
+            cout << "Stack Overflow\n";
+        }
+    }
+
+    // Pop from stack 1
+    int pop1() {
+        if (top1 >= 0)
+            return arr[top1--];
+        else {
+            cout << "Stack Underflow\n";
+            return -1;
+        }
+    }
+
+    // Pop from stack 2
+    int pop2() {
+        if (top2 < MAX)
+            return arr[top2++];
+        else {
+            cout << "Stack Underflow\n";
+            return -1;
+        }
+    }
+};
+
+// Main function
+int main() {
+    TwoStacks ts;
+
+    ts.push1(10);
+    ts.push1(20);
+    ts.push2(100);
+    ts.push2(200);
+
+    cout << "Popped from Stack 1: " << ts.pop1() << endl;
+    cout << "Popped from Stack 2: " << ts.pop2() << endl;
+
+    return 0;
+}
