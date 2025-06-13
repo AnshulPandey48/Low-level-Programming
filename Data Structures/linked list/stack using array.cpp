acbb391c07946533c36fd769dfdea780d28a8484
anshul <anshul4894@gmail.com>
2025-06-13 19:29:53 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 19:29:53

--- Data Structures/linked list/stack using array.cpp
+++ Data Structures/linked list/stack using array.cpp
@@ -1,6 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define max 100;
+#define max 100
 int stack[max];
 int top = -1;
 
@@ -12,4 +12,27 @@ int push(int x)
         return -1;
     }
     stack[++top] = x;
+}
+
+int pop()
+{
+    if (top == -1)
+    {
+        cout << "stack underflow\n";
+        return -1;
+    }
+    return stack[top--];
+}
+int peek()
+{
+    if (top == -1)
+    {
+        return -1;
+    }
+    return stack[top];
+}
+
+bool isempty()
+{
+    return top == -1;
 }
