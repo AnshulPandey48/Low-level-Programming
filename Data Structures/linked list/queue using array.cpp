a8ea4a7a8e51ed1741fcc0e4bd7ed1eb6b7b7920
anshul <anshul4894@gmail.com>
2025-06-13 23:21:35 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 23:21:35

---
+++ Data Structures/linked list/queue using array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#define max 100
+int queue[max];
+int front = -1, rear = -1;
+
+void enqueue(int x)
+{
+    if (rear == max - 1)
+    {
+        cout << "queue overflow\n";
+        return;
+    }
+    void enqueue()
+
+}
