364daf4cc64f84c1eaa254b6339fe19f36bf5a6f
anshul <anshul4894@gmail.com>
2025-06-13 19:58:48 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 19:58:48

--- Data Structures/linked list/stack using linked list.cpp
+++ Data Structures/linked list/stack using linked list.cpp
@@ -1,17 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct node{
+struct node
+{
     int data;
-    node*next;
-    node(int x){
+    node *next;
+    node(int x)
+    {
         data = x;
         next = nullptr;
     }
 };
-node*top = nullptr;
+node *top = nullptr;
 
-void push(int x){
-    node*newnode = new node(x);
-    newnode -> next = top;
+void push(int x)
+{
+    node *newnode = new node(x);
+    newnode->next = top;
     top = newnode;
+}
+int pop()
+{
+    if (top == nullptr)
+    {
+        cout << "underflow\n";
+        return -1;
+    }
+    node *temp = top;
+    int popedval = temp->data;
+    top = top->next;
+    delete temp;
+    return popedval;
+}
+int peek()
+{
+    if (top == nullptr)
+    {
+        cout << "empty stack\n";
+        return -1;
+    }
+    int peekedval = top->data;
+    return peekedval;
+}
+
+bool isempty()
+{
+    return top == -1;
 }
