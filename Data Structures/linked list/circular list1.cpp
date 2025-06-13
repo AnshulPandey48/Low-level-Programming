74726e726d073fed128a3e29b5930e0bef9819ac
anshul <anshul4894@gmail.com>
2025-06-13 18:13:14 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 18:13:14

---
+++ Data Structures/linked list/circular list1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = nullptr;
+    }
+};
+
+int main()
+{
+
+}
