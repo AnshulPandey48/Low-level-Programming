eafe1e6ecb49cfc149e7185d3e2ad1a1185384aa
anshul <anshul4894@gmail.com>
2025-06-13 23:41:47 +0530

    ⛓ Auto-commit from the depths at 2025-06-13 23:41:47

--- Data Structures/linked list/queue using linked list.cpp
+++ Data Structures/linked list/queue using linked list.cpp
@@ -10,4 +10,30 @@ struct node
         next = nullptr;
     }
 };
-node*front = nullptr, *rear = nullptr;
+node *front = nullptr, *rear = nullptr;
+node *enqueue(int x)
+{
+
+    node *temp = new node(x);
+    if (rear == nullptr)
+    {
+        front = rear = temp;
+    }
+    else
+    {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+int dequeue()
+{
+    if (front == nullptr)
+        return -1;
+    int val = front->data;
+    node *temp = front;
+    front = front->next;
+    if (front == nullptr)
+        rear = nullptr;
+    delete temp;
+    return val;
+}
