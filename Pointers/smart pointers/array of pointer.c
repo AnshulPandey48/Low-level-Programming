326087b7a88b52d15b91ccb008d94404f223878e
anshul <anshul4894@gmail.com>
2025-06-17 01:31:33 +0530

    ⛓ Auto-commit from the depths at 2025-06-17 01:31:33

---
+++ Pointers/smart pointers/array of pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a = 10, b = 20, c = 30;
+    int *ptr[3];
+    ptr[0] = &a;
+    ptr[1] = &b;
+    ptr[2] = &c;
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+}
