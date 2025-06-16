821c93cc50218d0e6ac361834baf239c0bf54442
anshul <anshul4894@gmail.com>
2025-06-17 00:40:30 +0530

    ⛓ Auto-commit from the depths at 2025-06-17 00:40:30

--- arrays/pointer_to_pointer.c
+++ arrays/pointer_to_pointer.c
@@ -4,9 +4,13 @@ int main()
 {
     int a = 10;
     int *ptr = &a;
-    printf("%d", *ptr);
-    int *q = &ptr;
-    printf("%d", *q);
+    printf("value : %d\n", *ptr); // value
+    int **q = &ptr;
+    printf("address of ptr : %p\n", *q); // prints address of ptr
+    printf("address of a : %p\n", ptr); // prints address of a
+    
+    printf("value of a : %d\n", **q); // prints value
+// a -> ptr -> q
     
     return 0;
 }
