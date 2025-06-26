883cbc4ac26d1d151ad86e2b485aced1aa208db5
anshul <anshul4894@gmail.com>
2025-06-26 06:01:51 +0530

    ⛓ Auto-commit from the depths at 2025-06-26 06:01:51

---
+++ OOPS/struct.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct employee{
+    int empid;
+    char empname[20];
+    char deparment[30];
+    int empsal;
+};
+void displaydetails(struct employee emp[],int n){
+    for(int i = 0 ; i <= n ; i++){
+        printf("sallary : %d",emp[i].empsal);
+        printf("id: %d",emp[i].empid);
+        printf("name: %s",emp[i].empname);
+        printf("department name: %s",emp[i].deparment);
+    }
+}
+int main(){
+    struct employee[30];
+}
